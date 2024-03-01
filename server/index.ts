1916ad876e187ba238ef5ab607aba66e01990999
farkuy <darkuy@mail.ru>
2024-03-01 18:40:03 +0300

    init server

---
+++ server/index.ts
@@ -0,0 +1,14 @@
+const express = require('express')
+const {graphqlHTTP} = require('express-graphql')
+const schema = require('./schema')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema: schema,
+
+}))
+app.listen(5000, () => console.log('server started on port 5000'))
---
+++ server/schema.ts
@@ -0,0 +1,28 @@
+const {buildSchema} = require('graphql')
+
+const schema = buildSchema(`
+    type User {
+        id: ID
+        username: String
+        age: Int
+        cash: Int
+    }
+    
+    input UserInput {
+        id: ID!
+        username: String!
+        age: Int!
+        cash: Int!
+    }
+    
+    type Query {
+        getAllUsers: [User]
+        getUser(id: ID): User
+    }
+    
+    type Mutation {
+        createUser(input: UserInput): User
+    }
+`)
+
+module.exports = schema
